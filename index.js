614ed6a4dad015e22ef88b577860d6f922017084
TheHacker894 <skylarmccauley864@gmail.com>
2017-12-16 19:32:38 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,65 @@
+const Discord = require("discord.js");
+const RichEmbed = require("discord.js").RichEmbed;
+const client = new Discord.Client();
+const data = require("./data/brain/data.json");
+const announcement = require("./data/brain/announcement.json");
+const game = require("./data/brain/game.json");
+const fs = require("fs");
+const prefix = data.prefix
+const request = require("request")
+const pusage = require('pidusage')
+
+client.on("message", (message) => {
+
+  const args = message.content.split(" ");
+  const command = message.content.split(" ")[0]
+  if(message.author.bot) return;
+  if(message.channel.type === 'dm') return;
+  if(!command.startsWith(prefix)) return;
+  const cmd = client.commands.get(command.slice(prefix.length))
+  if(cmd)
+    cmd.run(client, message, args, data, game, announcement)
+})
+    client.on("message", (message) => {
+      if(message.content.startsWith(`<@${client.user.id}>`)) {
+        var mentionedembed = new Discord.RichEmbed()
+          .setColor(data.embedcolor)
+          .setTitle('Prefix')
+          .setDescription('```' + prefix + '```')
+          message.channel.send({embed: mentionedembed})
+      }
+    })
+    client.on("message", function(message) {
+      var Attachment = (message.attachments).array();
+      Attachment.forEach(function(attachment) {
+        console.log(attachment.url);
+      })
+    })
+    
+    client.commands = new Discord.Collection();
+  fs.readdir("./data/commands", (err, files) => {
+    if(err) console.error(err)
+    const jsFiles = files.filter(f => f.split(".").pop() === "js")
+    if(jsFiles.length <= 0) {
+      console.log("No commands loaded")
+      return;
+    }
+    console.log('[Commands Loaded] ' + jsFiles.length)
+
+    jsFiles.forEach((f, i) => {
+      const props = require("./data/commands/" + f)
+      client.commands.set(props.help.name, props)
+    })
+  })
+      client.on("ready", () => {
+        console.log('Logged on as ' + client.user.id)
+        console.log('[Game] ' + game.game)
+        console.log('[Announcement] ' + announcement.announce)
+        client.user.setGame(game.game + ' | ' + data.prefix + 'help' )
+        pusage.unmonitor(process.pid)
+    
+      });
+      
+    
+    client.login(data.token)
+    
